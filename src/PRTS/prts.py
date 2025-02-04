89f1979b57e5fe7696a46630ad06988e4c4207b4
Tony32 <imtony32@gmail.com>
2025-02-04 21:10:17 +0800

    update prts tool

--- src/PRTS/prts.py
+++ src/PRTS/prts.py
@@ -11,12 +11,30 @@ licence: MIT
 """
 
 import requests
+from typing import Callable, Any
+
+class EventEmitter:
+    def __init__(self, event_emitter: Callable[[dict], Any] = None):
+        self.event_emitter = event_emitter
+
+    async def emit(self, description="Unknown State", status="in_progress", done=False):
+        if self.event_emitter:
+            await self.event_emitter(
+                {
+                    "type": "status",
+                    "data": {
+                        "status": status,
+                        "description": description,
+                        "done": done,
+                    },
+                }
+            )
 
 class Tools:
     def __init__(self):
         pass
 
-    def get_info(self,name: str) -> str:
+    async def get_info(self,name: str, __event_emitter__: Callable[[dict], Any] = None) -> str:
         """
         从PRTS获取干员/人物/组织/国家/地区的档案或(及)概要内容
         :param name: 干员/人物/组织/国家/地区名称
@@ -39,14 +57,45 @@ class Tools:
             return "\n".join(content) + "\n[作为语言模型你应该记住的]返回给用户时，保持人物设定，先总结内容，再根据自己的总结返回给用户\n[作为扮演的角色你应该记住的]你是从PRTS中获得的信息，返回给博士时遵守博士的指令"
         else:
             return "未找到该干员/人物档案(概要)"
-
-    def get_profile_enter(self,name: str) -> str:
+    
+    async def get_profile_enter(self,name: str, __event_emitter__: Callable[[dict], Any] = None) -> str:
+        emitter = EventEmitter(__event_emitter__)
+        await emitter.emit(f"正在搜索PRTS数据库...")
+        await emitter.emit(
+            status="in_progress",
+            description=f"正在搜索PRTS数据库...",
+            done=True
+        )
         url = f"https://prts.wiki/api.php?action=parse&page={name}&prop=wikitext&format=json"
-        response = requests.get(url)
+        try:
+            response = requests.get(url)
+        except:
+            await emitter.emit(
+            status="error",
+            description=f"无法连接到PRTS",
+            done=True
+            )
+            return None
         data = response.json()
         if 'error' in data:
             return None
         wikitext_content = data['parse']['wikitext']['*']
+        if '#redirect' in wikitext_content:
+            start = wikitext_content.find("[[") + 2
+            end = wikitext_content.find("]]")
+            redirect_target = wikitext_content[start:end]
+            url = f"https://prts.wiki/api.php?action=parse&page={redirect_target}&prop=wikitext&format=json"
+            try:
+                response = requests.get(url)
+            except:
+                await emitter.emit(
+                status="error",
+                description=f"无法连接到PRTS",
+                done=True
+                )
+                return None
+            data = response.json()
+            wikitext_content = data['parse']['wikitext']['*']
         section_title = "干员档案"
         start = f"=={section_title}==\n"
         start_index = wikitext_content.find(start)
@@ -61,11 +110,19 @@ class Tools:
             end_index = len(wikitext_content)
         section_content = wikitext_content[start_index:end_index]
         section_content = section_content.replace('{', '').replace('}', '').replace('|', '').replace('[[', '').replace(']]', '')
+        await emitter.emit(
+            status="complete",
+            description=f"完成PRTS数据库搜索",
+            done=True
+        )
         return f"该干员档案内容：\n{section_content}"
     
     def get_profile_noenter(self,name: str) -> str:
         url = f"https://prts.wiki/api.php?action=parse&page=剧情角色一览&prop=wikitext&format=json"
-        response = requests.get(url)
+        try:
+            response = requests.get(url)
+        except:
+            return None
         data = response.json()
         if 'error' in data:
             return None
@@ -87,11 +144,25 @@ class Tools:
     
     def get_organization_info(self,name: str) -> str:
         url = f"https://prts.wiki/api.php?action=parse&page=泰拉大典:组织/{name}&prop=wikitext&format=json"
-        response = requests.get(url)
+        try:
+            response = requests.get(url)
+        except:
+            return None
         data = response.json()
         if 'error' in data:
             return self.get_organization_info_2(name)
         wikitext_content = data['parse']['wikitext']['*']
+        if '#redirect' in wikitext_content:
+            start = wikitext_content.find("[[") + 2
+            end = wikitext_content.find("]]")
+            redirect_target = wikitext_content[start:end]
+            url = f"https://prts.wiki/api.php?action=parse&page={redirect_target}&prop=wikitext&format=json"
+            try:
+                response = requests.get(url)
+            except:
+                return None
+            data = response.json()
+            wikitext_content = data['parse']['wikitext']['*']
         start = f"{name}"
         start_index = wikitext_content.find(start)
         if start_index == -1:
@@ -107,7 +178,10 @@ class Tools:
         
     def get_organization_info_2(self,name: str) -> str:
         url = f"https://prts.wiki/api.php?action=parse&page=泰拉大典:组织/其他&prop=wikitext&format=json"
-        response = requests.get(url)
+        try:
+            response = requests.get(url)
+        except:
+            return None
         data = response.json()
         if 'error' in data:
             return None
@@ -129,11 +203,25 @@ class Tools:
     
     def get_place_info(self,name: str) -> str:
         url = f"https://prts.wiki/api.php?action=parse&page=泰拉大典:地理/{name}&prop=wikitext&format=json"
-        response = requests.get(url)
+        try:
+            response = requests.get(url)
+        except:
+            return None
         data = response.json()
         if 'error' in data:
             return self.get_place_info_2(name)
         wikitext_content = data['parse']['wikitext']['*']
+        if '#redirect' in wikitext_content:
+            start = wikitext_content.find("[[") + 2
+            end = wikitext_content.find("]]")
+            redirect_target = wikitext_content[start:end]
+            url = f"https://prts.wiki/api.php?action=parse&page={redirect_target}&prop=wikitext&format=json"
+            try:
+                response = requests.get(url)
+            except:
+                return None
+            data = response.json()
+            wikitext_content = data['parse']['wikitext']['*']
         start = f"{name}"
         start_index = wikitext_content.find(start)
         if start_index == -1:
@@ -149,7 +237,10 @@ class Tools:
         
     def get_place_info_2(self,name: str) -> str:
         url = f"https://prts.wiki/api.php?action=parse&page=泰拉大典:地理/其他&prop=wikitext&format=json"
-        response = requests.get(url)
+        try:
+            response = requests.get(url)
+        except:
+            return None
         data = response.json()
         if 'error' in data:
             return None
