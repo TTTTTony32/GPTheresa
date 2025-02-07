6f81b152aa80d0b3c8753963467592559ee7bc7e
Ashley Lee <nekokecore@emtips.net>
2025-02-07 16:39:22 +0800

    GPTheresa: fix entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 echo Copying data from temp to data.
-cp -R /openweb-ui-temp-data/* /openweb-ui-data/
-cp -R /gpt-sovits-temp-data/GPT_weights/* /GPT_weights/
-cp -R /gpt-sovits-temp-data/SoVITS_weights/* /SoVITS_weights/
-cp -R /gpt-sovits-temp-data/wavs/* /wavs/
+cp /openweb-ui-temp-data/webui.db /openweb-ui-data/webui.db
+cp /gpt-sovits-temp-data/GPT_weights/xiaote.ckpt /GPT_weights/xiaote.ckpt
+cp /gpt-sovits-temp-data/GPT_weights/xiaote.pth /GPT_weights/xiaote.pth
+cp /gpt-sovits-temp-data/SoVITS_weights/xiaote.pth /SoVITS_weights/xiaote.pth
+cp /gpt-sovits-temp-data/wavs/xiaote.wav /wavs/xiaote.wav
 exec "$@"
