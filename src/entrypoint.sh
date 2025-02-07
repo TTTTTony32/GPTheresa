dfc4df82700271e51c6c80ac1a0af04e9a3a11ae
Ashley Lee <nekokecore@emtips.net>
2025-02-07 20:16:29 +0800

    GPTheresa: fix docker compose

--- src/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo Copying data from temp to data.
-cp /openweb-ui-temp-data/webui.db /openweb-ui-data/webui.db
-cp /gpt-sovits-temp-data/GPT_weights/xiaote.ckpt /GPT_weights/xiaote.ckpt
-cp /gpt-sovits-temp-data/GPT_weights/xiaote.pth /GPT_weights/xiaote.pth
-cp /gpt-sovits-temp-data/SoVITS_weights/xiaote.pth /SoVITS_weights/xiaote.pth
-cp /gpt-sovits-temp-data/wavs/xiaote.wav /wavs/xiaote.wav
-exec "$@"
