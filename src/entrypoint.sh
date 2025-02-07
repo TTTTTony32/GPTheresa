6901cfeed4866b95173f0476b29bf86d53933ede
Ashley Lee <nekokecore@emtips.net>
2025-02-07 16:29:39 +0800

    GPTheresa: fix entrypoint.sh and docker compose

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+echo Copying data from temp to data.
 cp -R /openweb-ui-temp-data/* /openweb-ui-data/
 cp -R /gpt-sovits-temp-data/GPT_weights/* /GPT_weights/
 cp -R /gpt-sovits-temp-data/SoVITS_weights/* /SoVITS_weights/
