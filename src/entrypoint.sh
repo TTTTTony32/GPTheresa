403b5eec218d37ec0dccdfcdc3df2623ca370f79
Ashley Lee <nekokecore@emtips.net>
2025-02-07 15:50:50 +0800

    GPTheresa: fix Additional property is not allowed

---
+++ src/entrypoint.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+cp /openweb-ui-temp-data/webui.db /openweb-ui-data/webui.db
+cp -R /gpt-sovits-temp-data/GPT_weights /gpt-sovits-cpu-data/
+cp -R /gpt-sovits-temp-data/SoVITS_weights /gpt-sovits-gpu-data/
+cp -R /gpt-sovits-temp-data/wavs /gpt-sovits-cpu-data/
