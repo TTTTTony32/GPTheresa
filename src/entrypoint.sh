ae87fa91cf0fc5c861c92d152789389e1f4f09db
Ashley Lee <nekokecore@emtips.net>
2025-02-07 15:51:35 +0800

    GPTheresa: fix entrypoint.sh

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,3 +3,4 @@ cp /openweb-ui-temp-data/webui.db /openweb-ui-data/webui.db
 cp -R /gpt-sovits-temp-data/GPT_weights /gpt-sovits-cpu-data/
 cp -R /gpt-sovits-temp-data/SoVITS_weights /gpt-sovits-gpu-data/
 cp -R /gpt-sovits-temp-data/wavs /gpt-sovits-cpu-data/
+exec "$@"
